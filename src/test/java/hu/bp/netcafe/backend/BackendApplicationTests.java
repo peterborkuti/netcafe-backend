4b487d022fd2514c1df819d3a31e87dc8ea2e8b6
Peter Borkuti <peterborkuti@gmail.com>
2019-01-28 11:51:40 +0100

    testproperties

--- src/test/java/hu/bp/netcafe/backend/BackendApplicationTests.java
+++ src/test/java/hu/bp/netcafe/backend/BackendApplicationTests.java
@@ -3,14 +3,18 @@ package hu.bp.netcafe.backend;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@TestPropertySource(
+		locations = "classpath:application-test.properties")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BackendApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
 	}
 
 }

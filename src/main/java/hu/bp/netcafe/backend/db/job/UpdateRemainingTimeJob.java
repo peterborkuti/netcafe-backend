03fab4bed9b56bd884fc787139ce794bb2f6f1cc
Peter Borkuti <peterborkuti@gmail.com>
2019-01-29 11:28:22 +0100

    quartz is overcomlicated. Spring @Scheduled is enough and cleaner

--- src/main/java/hu/bp/netcafe/backend/db/job/UpdateRemainingTimeJob.java
+++ src/main/java/hu/bp/netcafe/backend/db/job/UpdateRemainingTimeJob.java
@@ -1,27 +1,20 @@
 package hu.bp.netcafe.backend.db.job;
 
-import hu.bp.netcafe.backend.db.entity.Device;
 import hu.bp.netcafe.backend.db.repository.DeviceRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
-import org.springframework.scheduling.quartz.QuartzJobBean;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 
+@Component
 @Slf4j
-public class UpdateRemainingTimeJob extends QuartzJobBean{
+public class UpdateRemainingTimeJob {
   @Autowired
   private DeviceRepository deviceRepository;
 
-  @Override
-  protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+  @Scheduled(fixedDelay = 2000)
+  public void decreaseTimeInDevices() {
     int updated = deviceRepository.decreaseRemainingTimeAllOnNet();
   }
 }

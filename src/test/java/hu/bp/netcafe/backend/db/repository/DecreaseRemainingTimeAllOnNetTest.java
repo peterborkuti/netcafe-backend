e2ff4e92d6b5cb183bfd827adf292b443dc5b129
Peter Borkuti <peterborkuti@gmail.com>
2019-01-29 11:32:39 +0100

    test for update job

--- src/test/java/hu/bp/netcafe/backend/db/repository/DecreaseRemainingTimeAllOnNetTest.java
+++ src/test/java/hu/bp/netcafe/backend/db/repository/DecreaseRemainingTimeAllOnNetTest.java
@@ -8,6 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -22,64 +26,72 @@ public class DecreaseRemainingTimeAllOnNetTest {
 	@Autowired
 	DeviceRepository deviceRepository;
 
+	@PersistenceContext
+	EntityManager entityManager;
+
+	private void setup(int remainingTime, boolean onNet) {
+		Device dev = new Device();
+		dev.setRemainingTime(remainingTime);
+		dev.setOnNet(onNet);
+
+		deviceRepository.saveAndFlush(dev);
+	}
+
+	private void runFunctionToTest(int expectedCount) {
+		int count = deviceRepository.decreaseRemainingTimeAllOnNet();
+
+		assertEquals(expectedCount, count);
+	}
+
 	@Test
 	public void shouldDecreaseTimeWhenOnNet() {
-		Device dev = new Device();
-		dev.setOnNet(true);
-		dev.setRemainingTime(2);
+		setup(2, true);
 
-		deviceRepository.save(dev);
+		runFunctionToTest(1);
 
-		deviceRepository.decreaseRemainingTimeAllOnNet();
-
-		Device read = deviceRepository.getOne(dev.getId());
+		Device read = deviceRepository.findAll().get(0);
 
 		assertEquals(1, read.getRemainingTime());
 		assertTrue(read.isOnNet());
 	}
 
+	@Test
 	public void shouldNotDecreaseTimeWhenNotOnNet() {
-		Device dev = new Device();
-		dev.setOnNet(false);
-		dev.setRemainingTime(2);
+		setup(2, false);
 
-		deviceRepository.save(dev);
+		runFunctionToTest(0);
 
-		deviceRepository.decreaseRemainingTimeAllOnNet();
-
-		dev = deviceRepository.getOne(dev.getId());
+		Device dev = deviceRepository.findAll().get(0);
 
 		assertEquals(2, dev.getRemainingTime());
 		assertFalse(dev.isOnNet());
 	}
 
+	@Test
 	public void shouldSetOnNetWhenRemainingTimeIsZeroAndDeviceWasOnNet() {
-		Device dev = new Device();
-		dev.setOnNet(true);
-		dev.setRemainingTime(1);
+		setup(1, true);
 
-		deviceRepository.save(dev);
+		runFunctionToTest(1);
 
-		deviceRepository.decreaseRemainingTimeAllOnNet();
-
-		dev = deviceRepository.getOne(dev.getId());
+		Device dev = deviceRepository.findAll().get(0);
 
 		assertEquals(0, dev.getRemainingTime());
 		assertFalse(dev.isOnNet());
 	}
 
+	/**
+	 * zero-check update runs only in case there was at least 1 device to decrease time.
+	 */
+	@Test
 	public void shouldSetOnNetAndDontGoBelowZeroRemainigTimeWhenRemainingTimeIsZeroAndDeviceWasOnNet() {
-		Device dev = new Device();
-		dev.setOnNet(true);
-		dev.setRemainingTime(0);
+		setup(1, true); // Hack for letting the zero-check run
+		setup(0, true);
 
-		deviceRepository.save(dev);
+		runFunctionToTest(1);
 
-		deviceRepository.decreaseRemainingTimeAllOnNet();
-
-		dev = deviceRepository.getOne(dev.getId());
-
-		assertEquals(0, dev.getRemainingTime());
-		assertFalse(dev.isOnNet());
+		deviceRepository.findAll().forEach(dev -> {
+			assertEquals(0, dev.getRemainingTime());
+			assertFalse(dev.isOnNet());
+		});
 	}
 }
